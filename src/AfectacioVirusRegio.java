bd0776df3910aec29c2977bc2b09b2991c700db2
Iria Auladell <u1990128@campus.udg.edu>
2025-04-01 18:13:48 +0200

    He començat a implementar part dels mètodes de la classe AfectacioVirusRegio, tot i que encara falta bastant per fer. M'he basat en el constructor, i he començat la d'avançar un dia i actualitzar tots els paràmetres. Encara falta molt per fer, però ja he començat a tenir una idea inicial de com fer-ho.

--- src/AfectacioVirusRegio.java
+++ src/AfectacioVirusRegio.java
@@ -20,6 +20,9 @@
  * @version 2025.3.13
  */
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class AfectacioVirusRegio {
 
     // ATRIBUTS (idea principal, falta canviar moltes coses segurament)
@@ -55,13 +58,32 @@ public class AfectacioVirusRegio {
 
     /**
      * Constructor de la classe AfectacioVirusRegio.
-     * @param virus Virus que afecta la regió.
-     * @param regio Regió que és infectada per el virus.
+     * @param v Virus que afecta la regió.
+     * @param r Regió que és infectada per el virus.
      */
-    public AfectacioVirusRegio(Virus virus, Regio regio, int numero_infectats) {
+    public AfectacioVirusRegio(Virus v, Regio r, int numero_infectats) {
         // Pre: virus ha de ser un objecte vàlid de la classe Virus, i regio ha de ser una regió
         // que existeixi.
         // Creem una zona concreta que està afectada per un virus concret.
+
+        // Inicialitzem els atributs
+        virus = v;
+        regio = r;
+
+        // Inicialitzem les llistes per als histogrames
+        _infectats = new ArrayList<>();
+        _malalts = new ArrayList<>();
+        _immunes = new ArrayList<>();
+        _contagiosos = new ArrayList<>();
+
+        // Inicialitzem les estadístiques acumulades
+        totalMalalts = 0;
+        totalMorts = 0;
+        totalContagis = 0;
+        totalInfectats = numero_infectats;
+
+        // Inicialitzem els infectats actuals a partir del número d'infectats que es passa com a paràmetre
+        _infectats.add(numero_infectats);
     }
 
     /**
@@ -71,6 +93,33 @@ public class AfectacioVirusRegio {
         // Pre: La simulació s'ha d'estar executant
         // Post: S'actualitza l'estat, calculant de nou els nous contagis, morts, persones immunes, segons
         // les característiques de la regió i del virus que està afectant.
+
+
+        // ---------------------- Calculo nous contagis --------------------
+        // Primer de tot, calculem nous contagis
+
+        // He decidit que la posició 0 de la llista _infectats representa el dia actual, és a dir, avui.
+        // Sempre que avancem un dia en la simulació, col·loquem els nous infectats al principi de la llista,
+        // i els elements que ja estaven es desplacen una posició cap a la dreta.
+        // Posició 0: Avui (els nous contagis que acabem de calcular), Posició 1: Ahir, Posició 2: Abans d'ahir (i així successivament...).
+        int nous_contagis = virus.nousContagis(_infectats.get(0));  // Utilitzem el mètode de Virus per calcular nous contagis
+        _infectats.add(0, nous_contagis);  // Els nous infectats es col·loquen al principi de la llista
+        totalInfectats = totalInfectats + nous_contagis;
+        totalContagis = totalContagis + nous_contagis;
+
+        // -------------------- Actualitzo els malalts --------------------
+        actualitzar_malalts();  // He decidit fer-ho amb un mètode privat perquè així tingui menys codi la funció i s'entengui més.
+
+        // -------------------- Actualitzem els immunes --------------------
+        actualitzar_immunes();  // També ho he passat a un mètode privat perquè quedi tot més clar.
+
+        // IDEA DE COSES QUE FALTEN PER FER:
+
+        // Per ara, només hem implementat les parts que controlen infectats, malalts i immunes.
+        // Encara falta:
+        // - Mirar com tenir constància dels contagiosos (si m'aclaro i tinc idea de com poder-los mantenir com a llista. Tot i que segurament els acabi fent com a acomulats).
+        // - Fer el càlcul de morts i de la seva distribució.
+
     }
 
     // Hem fet aquest mètode per poder mostrar l'estat en que es troba la regió infactada
@@ -86,8 +135,59 @@ public class AfectacioVirusRegio {
         // Post: Mostrem per pantalla el total de malalts, morts i contagis acumulats.
     }
 
+// ------------------------------------------------------------------------------------------------------------------------
 
-    // afegircasosvirus a la regio (metode d'afegir-lo)
-    // dos metodes dif (confinament dur i tou) i per desconfinament igual
+    // MÈTODES PRIVATS:
+
+    // Per el mètode d'avançar un dia
+
+    /**
+     * Actualitza la llista de malalts segons el temps d'incubació del virus.
+     * Si ja ha passat el temps d'incubació d'un infectat, aquest passa a ser malalt.
+     *
+     * Com que mantenim l'historial de cada estat (infectats, malalts, contagiosos, immunes)
+     * amb un esquema on la posició 0 representa el dia actual, hem de moure els infectats
+     * que ja han passat la incubació a la llista de malalts.
+     */
+    private void actualitzar_malalts() {
+    // Pre: La llista _infectats ha d'estar inicialitzada.
+    // Post: Si la llista _infectats és major que el temps d'incubació del virus, vol dir que les persones del primer dia ja han
+    // esdevingut malaltes i les movem de la llista _infectats a la llista malalts. Per tant, actualitzem la llista `malalts`
+    // amb el nombre de nous malalts al dia actual (posició 0). A més, també sumem aquests nous malalts al total de malalts acumulats.
+        if (_infectats.size() > virus.tempsIncubacio()) {
+            int nous_malalts = _infectats.remove(_infectats.size() - 1); // Traiem els infectats que ja han completat la incubació
+            _malalts.add(0, nous_malalts); // Els afegim a la llista de malalts al dia actual (posició 0)
+            totalMalalts += nous_malalts; // Actualitzem el total de malalts
+        } else {
+            _malalts.add(0, 0); // Si no hi ha nous malalts avui, afegim un 0 a la llista de malalts
+        }
+    }
+
+    /**
+     * Aquest mètode és per actualitzar la llista d'immunes segons el temps d'immunitat del virus.
+     * Quan un malalt ja ha passat el temps d'immunitat complet, aquest passa a ser considerat immune.
+     *
+     * Aquí, he considerat que el temps d'immunitat especificat pel virus (per exemple, 10 dies) indica
+     * que la persona no serà immune fins que hagi passat completament aquest temps. Per tant, hem de
+     * deixar passar aquests dies sencers i, només un cop els hagi completat, la considerarem immune.
+     *
+     * Per tant, jo he assumit que si per exemple tenim un temps d'immunitat de 10 dies, la persona només serà immune al dia següent,
+     * és a dir, al dia 11. */
+    private void actualitzar_immunes() {
+    // Pre: La llista _malalts ha d'estar inicialitzada i amb valors vàlids.
+    // Post: En el cas de que la llista malalts tingui tants dies registrats com el temps d'immunitat, llavors vol dir
+    // que els malalts del primer dia registrat ja han passat suficient temps, i ja els podem considerar inmunes.
+    // Per tant, els nous immunes els movem de la llista _malalts a la llista _immunes.
+
+        int temps_immunitat = virus.tempsImmunitat();
+
+        if (_malalts.size() > temps_immunitat) {
+            int nous_immunes = _malalts.remove(_malalts.size() - 1); // Els malalts que han superat la immunitat es consideren immunes.
+            _immunes.add(0, nous_immunes);  // Afegim els nous immunes a la posició 0 de la llista.
+        } else {
+            _immunes.add(0, 0); // Si no hi ha nous immunes avui, afegim un 0.
+        }
+    }
+
 }
 
