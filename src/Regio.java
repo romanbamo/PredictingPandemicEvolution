d9a7fcd6d2950eb50d3b07b56ee8c86bc7f26e80
Iria Auladell <u1990128@campus.udg.edu>
2025-05-08 21:36:40 +0200

    He començat a modificar i afegir mètodes i atributs a la classe regió. Crec que està molt relacionada amb la classe que he fet de AfectacioVirusRegio, ja que al final és la pròpia regió la que te constància de totes les seves Afectacions de cada virus, per tant, crec que puc ajudar en aquesta classe. Ara només he fet mètodes bàsics per poder afegir les regions veïnes en una llista i també les afectacions de cada virus que hi ha a la regió. El cap de setmana ja em posaré amb els mètodes que necessito per complementar la classe de AfectacioVirusRegió i així tenir tot el control de cada un dels virus presents en cada regió i les seves regions veïnes.

--- src/Regio.java
+++ src/Regio.java
@@ -1,3 +1,7 @@
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Regio {
 // F -> MIREIA, VAIG COMENTANT ENMIG DEL TEU CODI !
 
@@ -8,26 +12,50 @@ public class Regio {
      * Aquesta classe serveix per emmagatzemar informació sobre la població d’una regió
      * i els contactes que tenen les persones entre elles, un factor important en la propagació d’un virus.
      */
-    //ATRIBUTS
-    private String nom;
-    private int poblacio;
-    private boolean enConfinament;
-    private int casosActuals; // F-> AQUEST ATRIBUT NO ÉS NECESSARI, NO CAL AJUNTAR ELS CASOS DE TOTS ELS VIRUS
-    // Nombre de casos actius a la regió
-    // F-> ET FALTA LA TAXA DE INTERNA DE CONTACTES I TOT EL QUE FA REFERÈNCIA A REGIONS VEÏNES !
 
-    public Regio(String nom_regio, int poblacio_reg, boolean enConfinament, int casosActuals){ 
-    // F-> REVISAR EN FUNCIÓ DELS ATRIBUTS
-        this.nom = nom_regio;
-        this.poblacio = poblacio_reg;
-        this.enConfinament = false;
-        this.casosActuals = 0;
+
+
+    // --------------------------- ATRIBUTS DE LA CLASSE REGIO -------------------------------
+
+    private String nom; // Nom de la regió
+    private int poblacio; // Població total d’aquesta regió
+    private boolean enConfinament; // Ens serveix per a saber si aquesta regió es troba actualment en confinament
+
+    private double taxaContacteIntern;  // Probabilitat de contacte entre persones dins de la mateixa regió
+    private List<Regio> regionsVeines;  // Llista de regions amb les quals aquesta regió té contacte
+
+    private List<AfectacioVirusRegio> afectacions;
+    // Crec que aquest atribut és un dels més importants, perquè és regió la que ha de coneixer les seves propies
+    // afectacions. Perquè una regió pot estar infectada per varis virus, i de cada un nosaltres hem de tenir constància
+    // de com està la població durant cada dia.
+    // Per tant, per cada virus que està present a la regió, tindrem un objecte AfectacioVirusRegio que controla l’evolució.
+
+    /**
+     * Creem el constructor de la classe Regio.
+     *
+     * @param nom_regio Nom de la regió.
+     * @param poblacio_reg Població total que té la regió.
+     * @param taxa_contacte_interna Taxa de contacte entre persones dins de la mateixa regió.
+     */
+    public Regio(String nom_regio, int poblacio_reg, double taxa_contacte_interna) {
+        // Pre: nom_regio ha de ser un string vàlid, la població ha de ser positiva i la taxa entre 0 i 1 (ja que és una probabilitat).
+        // Post: Creem una nova regió amb la seva població, taxa de contacte interna, sense confinament inicial
+        // i amb cap afectació ni regió veïna assignada de moment.
+
+        nom = nom_regio;
+        poblacio = poblacio_reg;
+        taxaContacteIntern = taxa_contacte_interna;
+
+        enConfinament = false;  // Crec que quan la creem, la definim sense confinament i després ja se li
+        // assigna si en algun moment se li aplica un confinament.
+
+        // Inicialitzem les llistes buides (encara no sabem amb quines regions fa contacte ni quins virus té afectant-la)
+        regionsVeines = new ArrayList<>();
+        afectacions = new ArrayList<>();
     }
 
 
-    // MÈTODES
-
-    //con
+    // ----------------------------------------- MÈTODES PÚBLICS -------------------------------------------------------
 
     public String nom() {
         // Pre: La regió ha de tenir un nom vàlid.
@@ -35,17 +63,67 @@ public class Regio {
         return nom;
     }
 
-
-
     public int poblacio() {
         // Pre: La regió ha de tenir un nombre d'habitants vàlid.
         // Post: Retorna un enter que és el nombre d'habitants.
         return poblacio;
     }
 
-    public int casosActuals() { // F-> NO !
-        return casosActuals;
+    /**
+     * Aquest mètode l'he fet per poder indicar amb quines regions veïnes té contacte aquesta regió.
+     * És molt important perquè ho necessitarem després per calcular els contagis externs.
+     * A més, la taxa de contacte externa amb aquesta regió pot ser diferent per a cada veïna.
+     *
+     * @param veina Regió veïna amb la qual es manté contacte.
+     * @param taxaExternaContacte Taxa de contacte entre aquesta regió i la regió veïna (valor entre 0 i 1).
+     */
+    public void afegirRegioVeina(Regio veina, double taxaExternaContacte) {
+        // Pre: veina ha de ser una regió vàlida i diferent de this. La taxa ha de ser un valor entre 0 i 1.
+        // Post: Aquesta regió passa a tenir com a veïna la regió veina, amb la seva taxa de contacte.
+
+        // He fet això per assegurar-me de que no poguem dir-li a una regió que és veïna d'ella mateixa.
+        if (veina == this) {
+            throw new IllegalArgumentException("Una regió no pot ser veïna d’ella");
+        }
+
+        // Això també és important per assegurar-nos de que no repetim, ja que si ja hem dit que una regió és veiïna,
+        // doncs no podem tornar-la a afegir a la llista.
+        if (!regionsVeines.contains(veina)) {
+            regionsVeines.add(veina)
+        }
     }
+
+
+    /**
+     * Aquesta funció serveix per afegir una nova afectació a la regió.
+     * És a dir, quan arriba un nou virus, hem de crear una nova afectació perquè clar, el que volem és
+     * tenir control sobre com evoluciona aquest virus en la regió.
+     *
+     * El que he fet és basicament crear un objecte de la classre AfectacióVirusRegió (cridant el constructor)
+     * i llavors, doncs relacionar el virus amb la regió. I com ja hem comentat en la pròpia classe de
+     * AfectacioVirusRegió, doncs és necessari passar-li també el nombre d'infectats inicials.
+     * Després, per així tenir constància de l'evolució de cada virus que te la regió, doncs el que he fet és
+     * afegir-la a la llista d’afectacions.
+     */
+    public void afegirNovaAfectacio(Virus virus, int infectats_inicials) {
+        // Pre: El virus ha de ser vàlid i no ha d'existir ja una afectació seva en aquesta regió.
+        // infectats_inicials ha de ser major o igual a 0.
+        // Post: S’afegeix una nova afectació a la llista d’afectacions d’aquesta regió per controlar aquest virus.
+
+        AfectacioVirusRegio novaAfectacio = new AfectacioVirusRegio(virus, this, infectats_inicials);
+        afectacions.add(novaAfectacio);
+    }
+// PREGUNTA MEVA: QUAN UN VIRUS ACABA, TIPO JA DEIXA D'INFECTAR (Q AIXO NS COM ES TE CONSTÀNCIA), L'ELIMINEM DE LA LLISTA ?
+
+
+
+
+
+
+
+
+
+
 
     public void mostrarConfinament() {  
     // F-> IMPORTANT: CENTREU TOTES LES ENTRADES I SORTIDES (println) A LA CLASSE D'INTERACCIÓ. AQUEST MÈTODE NO TOCA AQUÍ 
